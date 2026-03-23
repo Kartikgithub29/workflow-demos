3ab494191867c52d41ae0c138a1edf338bdd4e35
Kartik Saxena <kartiksaxena648@gmail.com>
2026-03-23 17:47:27 +0530

    6

--- my-script.sh
+++ my-script.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 echo "My first github workflow command"
 ls
 cat index.html
+
+sudo apt update
 sudo apt install cowsay -y
-cowsay -f dragon "I am Caraxes" >> dragon.txt
+
+cowsay -f dragon "I am Caraxes" > dragon.txt
 cat dragon.txt
